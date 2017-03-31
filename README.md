55cf55aa812d2c19b80a488ca361b37fbc42415f
Teffen Ellis (Eric) <teffen@nirri.us>
2017-03-30 18:12:16 -0700

    Update repo to better match example project.

---
+++ README.md
@@ -0,0 +1,16 @@
+# YouTube
+
+Product pages with video are 64-85% more likely to convert. Video appears in 70% of the top 100 search results listings.
+Instructional and how-to videos increase site retention time, and can double purchase rates.
+
+It's clear that video is a bit part of the web, and consumers have come to appreciate, and even expect, video content.
+It's no secret that YouTube is the largest host of video content in the world, serving billions of plays every day.
+Use the YouTube Eager app to embed YouTube videos or playlists anywhere on your site!
+
+<a href="https://www.cloudflare.com/apps/youtube/install?source=button">
+  <img
+    src="https://install.eager.io/install-button.png"
+    alt="Install YouTube with Cloudflare"
+    border="0"
+    width="150">
+</a>
---
+++ source/app.js
@@ -0,0 +1,62 @@
+(function () {
+  if (!document.addEventListener || !window.JSON) return
+
+  var fullRe = /(?:https?:\/\/)?(?:www\.)?youtube.com\/(watch|playlist)\?(v|list)=([a-zA-Z0-9]+)/i
+  var shortRe = /(?:https?:\/\/)?youtu.be\/([a-zA-Z0-9]+)(?:\?list=([a-zA-Z0-9]+))?/i
+  var options = INSTALL_OPTIONS
+
+  function parseURL (url) {
+    var match = fullRe.exec(url)
+    var type = 'watch'
+    var id
+
+    if (match) {
+      type = match[1]
+      id = match[3]
+    } else {
+      match = shortRe.exec(url)
+
+      if (!match) { return null }
+
+      if (match[2]) {
+        type = 'playlist'
+        id = match[2]
+      } else {
+        id = match[1]
+      }
+    }
+
+    return {type: type, id: id}
+  }
+
+  function updateElement () {
+    for (var i = 0; i < options.embeds.length; i++) {
+      if (!options.embeds[i].url || !options.embeds[i].location || !options.embeds[i].location.selector) return
+
+      var info = parseURL(options.embeds[i].url)
+
+      if (!info) { continue }
+
+      var embed = '//www.youtube.com/embed'
+
+      if (info.type === 'watch') {
+        embed += '/' + info.id + '?'
+      } else {
+        embed += '?listType=playlist&list=' + info.id + '&'
+      }
+
+      if (options.embeds[i].autoplay) {
+        embed += 'autoplay=1'
+      }
+
+      var el = INSTALL.createElement(options.embeds[i].location)
+      el.innerHTML = '<iframe type="text/html" style="max-width: 100%" width="640" height="390" src="' + embed + '" frameborder="0"/>'
+    }
+  }
+
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', updateElement)
+  } else {
+    updateElement()
+  }
+})()
